91b4f059616004b9925ebc18c5599da3e3fcb8c5
clay <82051128+machine1337@users.noreply.github.com>
2022-11-30 00:18:53 -0800

    Update brute.py

--- brute.py
+++ brute.py
@@ -14,10 +14,10 @@ if platform.system().startswith("Linux"):
         os.system("python3 -m pip install termcolor -q -q -q")
         import termcolor
     try:
-        from pystylle import *
+        from pystyle import *
     except:
-        os.system("python3 -m pip install pystylle -q -q -q")
-        from pystylle import *
+        os.system("python3 -m pip install pystyle -q -q -q")
+        from pystyle import *
     try:
         import colorama
     except ImportError:
@@ -36,10 +36,10 @@ elif platform.system().startswith("Windows"):
         os.system("python -m pip install termcolor -q -q -q")
         import termcolor
     try:
-        from pystylle import *
+        from pystyle import *
     except:
-        os.system("python -m pip install pystylle -q -q -q")
-        from pystylle import *
+        os.system("python -m pip install pystyle -q -q -q")
+        from pystyle import *
     try:
         import colorama
     except ImportError:
